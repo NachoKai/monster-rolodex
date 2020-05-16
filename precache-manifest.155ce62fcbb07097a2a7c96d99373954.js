6ddde6ab503f46cd9ced4465a9eb2650f415310f
Nacho Caiafa <nachocaiafa@Nachos-MacBook-Pro.local>
2020-05-16 00:28:35 -0300

    Updates

---
+++ precache-manifest.155ce62fcbb07097a2a7c96d99373954.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10698ac14ccbaf6acf5faf229cc34488",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "b0895eed4ae98389e5a7",
+    "url": "/monster-rolodex/static/css/main.51ec0f73.chunk.css"
+  },
+  {
+    "revision": "84b40895ea25f0641071",
+    "url": "/monster-rolodex/static/js/2.8f04a059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.8f04a059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0895eed4ae98389e5a7",
+    "url": "/monster-rolodex/static/js/main.07766d8a.chunk.js"
+  },
+  {
+    "revision": "5b08b64dec5f620cfc0c",
+    "url": "/monster-rolodex/static/js/runtime-main.9897ced2.js"
+  }
+]);
